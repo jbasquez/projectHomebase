867f0b478f87a7a483988f3da7f9c540fe7505db
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 16:04:50 -0400

    activities component added and called on

---
+++ project-homebase/src/components/activities.js
@@ -0,0 +1,17 @@
+import React from 'react';
+function Activities() {
+    return (
+      <div >
+          <h2>Your new Activity</h2>
+        <div className="row">
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+            
+          </div>
+          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Activities;
--- project-homebase/src/pages/home.js
+++ project-homebase/src/pages/home.js
@@ -1,22 +1,23 @@
 import React from "react";
+import Activities from "../components/activities"
 
 function Home() {
     return (
       <div >
         <div className="row">
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-          <h2>Your activities</h2>
+            <Activities />
           </div>
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-          <h2>New listings</h2>
+            <h2>New listings</h2>
           </div>
         </div>
         <div className="row">
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-          <h2>Accomplishments</h2>
+            <h2>Accomplishments</h2>
           </div>
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-          <h2>Articles</h2>
+            <h2>Articles</h2>
           </div>
         </div>
       </div>
