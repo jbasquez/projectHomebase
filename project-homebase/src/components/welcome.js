f3cb40067fe5a497096c9c55897a6f915b2eb364
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 14:15:40 -0400

    added new photo trying to style it

--- project-homebase/src/components/welcome.js
+++ project-homebase/src/components/welcome.js
@@ -4,12 +4,14 @@ import '../css/welcome.css';
 function Welcome() {
     return (
       <div className="home">
-        <div className="row">
+        <div className="row contentwrapper">
             <div className="col-lg-6 col-md-8 col-sm-10 welcomeMessage">
-                <h2>Welcome to Home Base!</h2>
-                <p>Mot0:hdfjakldasfj</p>
-                <p>about:</p>
-                <p>buttons here</p>
+                <div className="welcomeImg">
+                    <h2>Welcome to Home Base!</h2>
+                    <p>Moto:hdfjakldasfj</p>
+                    <p>about:</p>
+                    <p>buttons here</p>
+                </div>
             </div>
         </div>
       </div>
