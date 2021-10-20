fc1876c19f1cde01c96a3b6f453f74ffc91beced
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 13:51:47 -0400

    rough styling for welcome pag

--- project-homebase/src/components/welcome.js
+++ project-homebase/src/components/welcome.js
@@ -1,16 +1,14 @@
 import React from 'react';
+import '../css/welcome.css';
 
 function Welcome() {
     return (
       <div className="home">
-          <div className="row">
-          <div className="col-lg-8 col-md-8 col-sm-8">
-            <h2>123</h2>
+        <div className="row">
+            <div className="col-lg-8 col-md-8 col-sm-8 welcomeMessage">
+                <h2>123</h2>
+            </div>
         </div>
-        <div className="col-lg-4 col-md-4 col-sm-8">
-            <h2>123</h2>
-        </div>
-          </div>
       </div>
     );
   }
