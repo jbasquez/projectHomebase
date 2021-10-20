cff842fb080ad3c51b78e78e294cbbd9313d589a
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 15:22:51 -0400

    polishing sizing and styling for image

--- project-homebase/src/components/welcome.js
+++ project-homebase/src/components/welcome.js
@@ -5,14 +5,16 @@ function Welcome() {
     return (
       <div>
         <div className="row">
-            <div className="col-lg-4 col-md-8 col-sm-10 col-xs-10">
-                <div className="wrapper">
-                    <div className=" welcomeMessage welcomeImg">
-                        <div className="welcomeWidth">
-                            <h2>Welcome to Home Base!</h2>
-                            <p>Moto:hdfjakldasfj</p>
-                            <p>about:</p>
-                            <p>buttons here</p>
+            <div className="center col-lg-12 col-md-8 col-sm-10 col-xs-10">
+                <div className="container">
+                    <div className="wrapper">
+                        <div className=" welcomeMessage welcomeImg">
+                            <div className="welcomeWidth">
+                                <h2>Welcome to Home Base!</h2>
+                                <p>Moto:hdfjakldasfj</p>
+                                <p>about:</p>
+                                <p>buttons here</p>
+                            </div>
                         </div>
                     </div>
                 </div>
