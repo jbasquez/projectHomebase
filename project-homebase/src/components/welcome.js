6a9d99795634620221665a2b88fd1c9d84347337
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 15:28:02 -0400

    some font styling

--- project-homebase/src/components/welcome.js
+++ project-homebase/src/components/welcome.js
@@ -8,12 +8,12 @@ function Welcome() {
             <div className="center col-lg-12 col-md-8 col-sm-10 col-xs-10">
                 <div className="container">
                     <div className="wrapper">
-                        <div className=" welcomeMessage welcomeImg">
+                        <div className="welcomeMessage welcomeImg">
                             <div className="welcomeWidth">
-                                <h2>Welcome to Home Base!</h2>
-                                <p>Moto:hdfjakldasfj</p>
-                                <p>about:</p>
-                                <p>buttons here</p>
+                                <h2 className="welcomeText">Welcome to Home Base!</h2>
+                                <p className="welcomeText">Moto:hdfjakldasfj</p>
+                                <p className="welcomeText">about:</p>
+                                <p className="welcomeText">buttons here</p>
                             </div>
                         </div>
                     </div>
