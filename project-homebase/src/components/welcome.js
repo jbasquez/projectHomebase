2833a03912d3922dc7e44303389ef994cae92f95
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 14:49:38 -0400

    used wrapper to fix bug for words not staying on picture

--- project-homebase/src/components/welcome.js
+++ project-homebase/src/components/welcome.js
@@ -6,12 +6,14 @@ function Welcome() {
       <div>
         <div className="row">
             <div className="col-lg-6 col-md-8 col-sm-10 col-xs-10 welcomeMessage">
-                <div className="welcomeImg">
-                    <div className="welcomeWidth">
-                        <h2>Welcome to Home Base!</h2>
-                        <p>Moto:hdfjakldasfj</p>
-                        <p>about:</p>
-                        <p>buttons here</p>
+                <div className="wrapper">
+                    <div className="welcomeImg">
+                        <div className="welcomeWidth">
+                            <h2>Welcome to Home Base!</h2>
+                            <p>Moto:hdfjakldasfj</p>
+                            <p>about:</p>
+                            <p>buttons here</p>
+                        </div>
                     </div>
                 </div>
             </div>
