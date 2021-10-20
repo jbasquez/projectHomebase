f300c82a6b2371cb5abd7df97a52587a106c7091
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 13:27:12 -0400

    skelton for home page

--- project-homebase/src/pages/home.js
+++ project-homebase/src/pages/home.js
@@ -2,7 +2,14 @@ import React from "react";
 function Home() {
     return (
       <div className="home">
-        <p>helloooo</p>
+          <div className="row">
+          <div className="col-lg-8 col-md-8 col-sm-8">
+            <h2>hello</h2>
+        </div>
+        <div className="col-lg-4 col-md-4 col-sm-8">
+            <h2>hello</h2>
+        </div>
+          </div>
       </div>
     );
   }
