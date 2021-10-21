f60c5c1a7daf1b3454bbb97fa8fd76539b55b597
Joe Basquez <jybasquez@yahoo.com>
2021-10-21 17:31:37 -0400

    added in box

--- project-homebase/src/pages/home.js
+++ project-homebase/src/pages/home.js
@@ -5,21 +5,23 @@ import "../css/home.css";
 function Home() {
     return (
       <div>
+        <div className="homeBox">
         <div className="row">
-          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <div className="col-lg-2 col-md-6 col-sm-4 col-xs-12">
             <Activities />
           </div>
-          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <div className="col-lg-2 col-md-6 col-sm-4 col-xs-12">
             <h2>Lessons</h2>
           </div>
         </div>
         <div className="row">
-          <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+          <div className="col-lg-2 col-md-6 col-sm-6 col-xs-12">
             <h2>Accomplishments</h2>
           </div>
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
             <h2>Articles</h2>
           </div>
+        </div>
         </div>
       </div>
     );
