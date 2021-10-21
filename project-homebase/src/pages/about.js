9807b662734227b78ceabed8087f49314fcb8298
Joe Basquez <jybasquez@yahoo.com>
2021-10-20 20:14:02 -0400

    setting up text styling

--- project-homebase/src/pages/about.js
+++ project-homebase/src/pages/about.js
@@ -7,8 +7,8 @@ function About() {
         <div className="aboutBg">
         <div className="row">
           <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-              <h2 className="">About Your Teacher</h2>
-              <p className="aboutText">Hi! My name is Mitzie Newman. I've been in real estate here in Fayetteville, NC, since 2005 and am now licensed in Wisconsin again as well! I've dealt with many facets of real estate, whether it be property management, assisting a client with purchasing a home or selling a home as well as short sales, foreclosures, and buy and hold or buy and sell investing.
+              <h2 className="aboutHeader">About Your Teacher</h2>
+              <p className="aboutBio">Hi! My name is Mitzie Newman. I've been in real estate here in Fayetteville, NC, since 2005 and am now licensed in Wisconsin again as well! I've dealt with many facets of real estate, whether it be property management, assisting a client with purchasing a home or selling a home as well as short sales, foreclosures, and buy and hold or buy and sell investing.
                 My friends, family and clients agree that I have a passion for real estate. Tell me what you need and I'll be there to help! I love helping my clients and love a good challenge!
                 I'm the person to talk to whether you are looking to buy (I've helped buyers buy site unseen!), selling (I’ve done numerous regular sales as well short sales and foreclosures - you need someone with experience when dealing with those!) or need property management (unfortunately right now for many sellers it is a necessary evil!).
                 Since going out on my own as an agent in 2015, I’ve won numerous awards, accolades and designations.</p>
