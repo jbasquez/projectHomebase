e3eb5892ce50fd942eea26cbeed4711d96994edb
Joe Basquez <jybasquez@yahoo.com>
2021-10-21 18:39:16 -0400

    added 5 props to lessons as place holders

--- project-homebase/src/pages/lessonpg.js
+++ project-homebase/src/pages/lessonpg.js
@@ -17,7 +17,7 @@ function Lessons() {
             />
           </div>
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-            {/* <LessonCard 
+          <LessonCard 
               id={lesson[1].id}
               name={lesson[1].name}
               about={lesson[1].about}
@@ -25,7 +25,7 @@ function Lessons() {
               image={lesson[1].image}
               start={lesson[1].start}
               repo={lesson[1].repo}
-            /> */}
+            />
           </div>
           <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
             <LessonCard 
@@ -41,43 +41,39 @@ function Lessons() {
         </div>
           <div className="row bottom mar">
             <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-              {/* <LessonCard 
-                id={lesson[3].id}
-                name={lesson[3].name}
-                about={lesson[3].about}
-                time={lesson[3].time}
-                image={lesson[3].image}
-                start={lesson[3].start}
-                repo={lesson[3].repo}
-              /> */}
+            <LessonCard 
+              id={lesson[1].id}
+              name={lesson[1].name}
+              about={lesson[1].about}
+              time={lesson[1].time}
+              image={lesson[1].image}
+              start={lesson[1].start}
+              repo={lesson[1].repo}
+            />
             </div>
             <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-              {/* <LessonCard 
-                  id={lesson[5].id}
-                  name={lesson[5].name}
-                  about={lesson[5].about}
-                  time={lesson[5].time}
-                  image={lesson[5].image}
-                  start={lesson[5].start}
-                  repo={lesson[5].repo}
-                /> */}
+            <LessonCard 
+              id={lesson[1].id}
+              name={lesson[1].name}
+              about={lesson[1].about}
+              time={lesson[1].time}
+              image={lesson[1].image}
+              start={lesson[1].start}
+              repo={lesson[1].repo}
+            />
             </div>
             <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
               <div>
-              {/* <LessonCard 
-                id={lesson[4].id}
-                name={lesson[4].name}
-                about={lesson[4].about}
-                time={lesson[4].time}
-                image={lesson[4].image}
-                start={lesson[4].start}
-                repo={lesson[4].repo}
-              /> */}
+              <LessonCard 
+              id={lesson[1].id}
+              name={lesson[1].name}
+              about={lesson[1].about}
+              time={lesson[1].time}
+              image={lesson[1].image}
+              start={lesson[1].start}
+              repo={lesson[1].repo}
+            />
               </div>
-              {/* <div className="square">
-                <h2 className="headers textColor">Click to see more lessons and repos on github</h2>
-              <a className="btn PrimaryBtn contact mar" href="https://github.com/jbasquez?tab=repositories" target="_blank" rel="noopener noreferrer">Click Now!</a>
-              </div> */}
             </div>
           </div>
 
