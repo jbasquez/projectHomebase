3954417b7bf836653e0009f75620c4b76cd57796
Joe Basquez <jybasquez@yahoo.com>
2021-10-22 21:44:41 -0400

    added in brochure and required signature link

--- project-homebase/src/components/unit.js
+++ project-homebase/src/components/unit.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import quiz from '../units.json';
 import Unit from '../props/unit';
+import {Link} from 'react-router-dom';
 
 function Unit1() {
     return (
@@ -13,6 +14,12 @@ function Unit1() {
               lesson={quiz[0].lesson}
             />
           </div>
+        </div>
+        <div className="row">
+        <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12">
+        <Link to={{ pathname: "https://www.ncrec.gov/" }} target="_blank">Brochure</Link>
+        <Link to={{ pathname: "https://www.ncrec.gov/Brochures/WWREADisclosureForm.pdf" }} target="_blank">Sign Here</Link>
+        </div>
         </div>
       </div>
     );
