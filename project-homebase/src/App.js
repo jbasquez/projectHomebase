49aaafde64272a4a026095dd6c578569ff638226
Joe Basquez <jybasquez@yahoo.com>
2021-10-22 20:06:18 -0400

    added in new json url for unit1 created unit1 page and switch command for it

--- project-homebase/src/App.js
+++ project-homebase/src/App.js
@@ -6,6 +6,7 @@ import Welcome from "./components/welcome";
 import About from "./pages/about";
 import "../src/css/home.css";
 import Lessons from "../src/pages/lessonpg";
+import Unit1 from "../src/components/unit";
 
 function App() {
     return (
@@ -17,6 +18,7 @@ function App() {
               <Route exact path={"/welcome"} component={Welcome} />
               <Route exact path={"/about"} component={About} />
               <Route exact path={"/lessons"} component={Lessons} />
+              <Route exact path={"/unit1"} component={Unit1} />
             </Switch>
           </Router>
       </div>
---
+++ project-homebase/src/components/unit.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Unit1() {
+    return (
+      <div>
+        <div className="homeBox">
+        <div className="row">
+          <div className="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+          </div>
+          <div className="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+            <h2>Lessons</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+            <h2>Accomplishments</h2>
+          </div>
+          <div className="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+            <h2>Articles</h2>
+          </div>
+        </div>
+        </div>
+      </div>
+    );
+  }
+
+  export default Unit1;
