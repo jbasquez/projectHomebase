22bc4f058727fbee4822857db848d2d63d12ab3f
Joe Basquez <jybasquez@yahoo.com>
2021-10-23 15:28:54 -0400

    new navbar mobile ready

--- project-homebase/src/components/navBar.js
+++ project-homebase/src/components/navBar.js
@@ -3,17 +3,12 @@ import {Link} from 'react-router-dom';
 
 function NavBar(){
     return(
-        <nav className="navbar navbar-expand-sm navbar-light bg-light">
-            <div className="navbar-brand"></div>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <Link to="/" className="nav-link">Home</Link>
-                    <Link to="/lessons" className="nav-link">Lessons</Link>
-                    <Link to="/projects" className="nav-link">Account</Link>
-                    <Link to="/about" className="nav-link">About</Link>
-                </ul>
-            </div>
-        </nav>
+        <nav className="navbar navbar-expand-lg navbar-expand-md navbar-expand-sm navbar-light bg-light">
+        <a className="navbar-brand col-lg-1 col-md-1 col-sm-2 col-xs-12" href="/">Home</a>
+        <Link to="/lessons" className="navbar-brand col-lg-1 col-md-1 col-sm-3 col-xs-12">Lessons</Link>
+        <Link to="/projects" className="navbar-brand col-lg-1 col-md-1 col-sm-3 col-xs-12">Account</Link>
+        <Link to="/about" className="navbar-brand col-lg-1 col-md-1 col-sm-3 col-xs-12">About</Link>
+      </nav>
     )
 }
 export default NavBar;
