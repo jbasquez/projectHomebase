c019c55ab15c2278bc529f7d287bab3e10b953a2
Joe Basquez <jybasquez@yahoo.com>
2021-10-23 14:11:08 -0400

    added button for brochure basic styling added and connected css page for unit

--- project-homebase/src/components/unit.js
+++ project-homebase/src/components/unit.js
@@ -2,6 +2,7 @@ import React from 'react';
 import quiz from '../units.json';
 import Unit from '../props/unit';
 import {Link} from 'react-router-dom';
+import "../css/unit.css";
 
 function Unit1() {
     return (
@@ -17,7 +18,7 @@ function Unit1() {
         </div>
         <div className="row">
         <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12">
-        <Link to={{ pathname: "https://www.ncrec.gov/" }} target="_blank">Brochure</Link>
+        <Link to={{ pathname: "https://www.ncrec.gov/" }} target="_blank" className="unitBtn1">Brochure</Link>
         <Link to={{ pathname: "https://www.ncrec.gov/Brochures/WWREADisclosureForm.pdf" }} target="_blank">Sign Here</Link>
         </div>
         </div>
