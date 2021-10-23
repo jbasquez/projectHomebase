7ab5b10b891dfa6d76414ffca404ec8d3d62689e
Joe Basquez <jybasquez@yahoo.com>
2021-10-22 21:29:43 -0400

    starting to style first unit

--- project-homebase/src/components/unit.js
+++ project-homebase/src/components/unit.js
@@ -5,11 +5,15 @@ import Unit from '../props/unit';
 function Unit1() {
     return (
       <div>
-        <Unit
+        <div className="row">
+          <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12">
+            <Unit
               id={quiz[0].id}
               name={quiz[0].name}
               lesson={quiz[0].lesson}
             />
+          </div>
+        </div>
       </div>
     );
   }
