4508687dd28841ceff2b70eab7c7d76d7279015c
Joe Basquez <jybasquez@yahoo.com>
2021-10-28 19:00:39 -0400

    unit 2 to 3 link button added

--- project-homebase/src/components/units/unit2.js
+++ project-homebase/src/components/units/unit2.js
@@ -23,7 +23,7 @@ function Unit2() {
         </div>
         </div>
         <div className="row">
-        <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12">
+        <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12 box">
         <div>
           <p>here are a few links to search homes, learn about the role an agent can play in a transaction, as well as if you are ready to start the process, there's a buyer interview sheet for you to complete at your convenience!</p>
         </div>
@@ -38,7 +38,7 @@ function Unit2() {
         <div className="col-lg-4 col-md-4 col-sm-10 col-xs-12">
         <div>
         <Link to={{ pathname: "https://www.raefordnchomes.com/" }} target="_blank" className="unitBtn1">Start Your Home Search</Link>
-        <Link to="/unit2" className="unitBtn1">Next Unit</Link>
+        <Link to="/unit3" className="unitBtn1">Next Unit</Link>
         </div>
         </div>
         </div>
