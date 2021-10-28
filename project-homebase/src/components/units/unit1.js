dd744f12a7ef1d2d7b057821dc8f40af2affb824
Joe Basquez <jybasquez@yahoo.com>
2021-10-28 18:39:53 -0400

    added background to text for unit 1 fixing sizing bug for video and buttons

--- project-homebase/src/components/units/unit1.js
+++ project-homebase/src/components/units/unit1.js
@@ -26,19 +26,19 @@ function Unit1() {
         </div>
         </div>
         <div className="row">
-        <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12">
+        <div className="col-lg-8 col-md-8 col-sm-10 col-xs-12 box">
         <div>
           <p>here are a few links to search homes, learn about the role an agent can play in a transaction, as well as if you are ready to start the process, there's a buyer interview sheet for you to complete at your convenience!</p>
         </div>
         </div>
         </div>
         <div className="row">
-        <div className="col-lg-4 col-md-4 col-sm-10 col-xs-12">
+        <div className="col-lg-4 col-md-5 col-sm-12 col-xs-12">
         <div>
         <iframe title="video1" width="420" height="315"src="https://www.youtube.com/embed/p6bLCudobFQ"></iframe>
         </div>
         </div>
-        <div className="col-lg-4 col-md-4 col-sm-10 col-xs-12">
+        <div className="col-lg-4 col-md-12 col-sm-12 col-xs-12">
         <div>
         <Link to={{ pathname: "https://www.raefordnchomes.com/" }} target="_blank" className="unitBtn1">Start Your Home Search</Link>
         <Link to={{ pathname: "https://fs3.formsite.com/NCRealEstate/form9/index.html" }} target="_blank" className="unitBtn1">Start Your Home Buying process</Link>
